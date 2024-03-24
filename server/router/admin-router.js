888e8200c36ff5da0470601a3749c78deeaa5abd
Akash <manirujjamanandlion@gmail.com>
2024-03-24 21:49:00 +0530

    Middleware for checking if the user has token or not (logged in or not)

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const adminController  = require("../controllers/admin-controller");
+const adminController = require("../controllers/admin-controller");
 const router = express.Router();
+const authMiddleware = require("../middlewares/auth-middleware");
 
-
-router.route('/users').get(adminController.getAllUsers);
-router.route('/contacts').get(adminController.getAllContacts);
+router.route("/users").get(authMiddleware, adminController.getAllUsers);
+router.route("/contacts").get(authMiddleware, adminController.getAllContacts);
 
 module.exports = router;
