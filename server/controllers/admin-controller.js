d18bc8c8451bd096143c4033a3960d7376a92cff
Akash <manirujjamanandlion@gmail.com>
2024-03-24 21:37:41 +0530

    getting all contact information from db

--- server/controllers/admin-controller.js
+++ server/controllers/admin-controller.js
@@ -1,7 +1,8 @@
 const User = require("../models/user-model");
+const Contact = require("../models/contact-model");
 const getAllUsers = async (req, res) => {
   try {
-    const users = await User.find({isAdmin:true},{password:0});
+    const users = await User.find({ isAdmin: true }, { password: 0 });
     // const users = await User.find({},{password:0});
     if (!users || users.length === 0)
       return res.status(404).json({ message: "No users Found" });
@@ -11,4 +12,15 @@ const getAllUsers = async (req, res) => {
   }
 };
 
-module.exports = getAllUsers;
+const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    if (!contacts || contacts.length === 0)
+      return res.status(404).json({ message: "No Contacts Found" });
+    return res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getAllUsers, getAllContacts };
