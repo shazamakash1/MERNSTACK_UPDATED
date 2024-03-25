a0f6b5883c25f9b14514c7ef518d4e3ee7933d4e
Akash <manirujjamanandlion@gmail.com>
2024-03-25 17:27:21 +0530

    admin contact page

--- client/src/pages/Admin-Contacts.jsx
+++ client/src/pages/Admin-Contacts.jsx
@@ -1,3 +1,54 @@
-export const AdminContacts = () =>{
-    return <h1>Admin Contacts Pannel</h1>
-}
+import { useEffect, useState } from "react";
+import { useAuth } from "../store/auth";
+import { toast } from "react-toastify";
+
+export const AdminContacts = () => {
+  const { authorizationToken } = useAuth();
+  const [contactData, setContactData] = useState([]);
+
+  const getContactsData = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/admin/contacts", {
+        method: "GET",
+        headers: {
+          Authorization: authorizationToken,
+        },
+      });
+
+      const data = await response.json();
+      //   console.log('Contact Data',data);
+      if (response.ok) {
+        setContactData(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getContactsData();
+  }, []);
+
+  return (
+    <>
+      <section className="admin-users-section">
+        <div className="container">
+          <h1>Admin Contact Data</h1>
+        </div>
+        <div className="container admin-users">
+          {contactData.map((currContactData, index) => {
+            const { username, email, message } = currContactData;
+            return (
+              <div key={index}>
+                <p>{username}</p>
+                <p>{email}</p>
+                <p>{message}</p>
+                <button className="btn">Delete</button>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
