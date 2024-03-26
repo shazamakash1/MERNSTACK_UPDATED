5bdf9376b532f98051dc9c8d1d9a45d1279bad9c
shazamakash1 <manirujjamanandlion@gmail.com>
2024-03-26 06:38:38 +0000

    cors fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api':import.meta.env.VITE_APP_URI_API,
+    }
+
+  },
   plugins: [react()],
 })
