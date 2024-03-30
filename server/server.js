7aa15b13613fcdf7a86eca4ec965ec5be794f6a3
Akash <manirujjamanandlion@gmail.com>
2024-03-30 16:20:51 +0530

    minor fixes

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 const authRoute = require("./router/auth-router");
 const contactRoute = require("./router/contact-router");
 const serviceRoute = require("./router/service-router");
 const connectDb = require("./utils/db");
-const errorMiddleware = require('./middlewares/error-middleware');
+const errorMiddleware = require("./middlewares/error-middleware");
 const adminRoute = require("./router/admin-router");
 //implementing CORS - handling the issue
 
 const corsOptions = {
-    origin: process.env.FRONTEND_ORIGIN,
-    methods:"GET,POST,PUT,DELETE,PATCH,HEAD",
-    credentials:true,
+  origin: [process.env.FRONTEND_ORIGIN],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
+  credentials: true,
 };
 
 // const corsOptions = {
@@ -31,20 +31,20 @@ app.use(cors(corsOptions));
 //pls comment
 
 app.use(express.json()); //adds an express middleware, will handle json request body
-//Mount the router to the app, also use mount 
+//Mount the router to the app, also use mount
 app.use("/api/auth", authRoute);
 app.use("/api/form", contactRoute);
 app.use("/api/data", serviceRoute);
 
 //admin root
-app.use("/api/admin",adminRoute);
+app.use("/api/admin", adminRoute);
 
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT;
 
-connectDb().then(() =>{
-    app.listen(PORT , ()=>{
-        console.log(`Server is running at port ${PORT}`);
-    }); 
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+  });
 });
