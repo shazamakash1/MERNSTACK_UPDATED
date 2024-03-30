94624db4f31a3734e8b69012183d6604856f3afd
Akash <manirujjamanandlion@gmail.com>
2024-03-30 16:16:54 +0530

    front end deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':import.meta.env.VITE_APP_URI_API,
-    }
-
-  },
   plugins: [react()],
 })
